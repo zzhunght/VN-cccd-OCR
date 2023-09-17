e18bfa736e797d6ff1e634f29873e4b3a8f74d86
ZettourNguyen <121755160+ZettourNguyen@users.noreply.github.com>
2023-09-17 19:33:05 +0700

    complete excel and 1 word

--- main.py
+++ main.py
@@ -126,3 +126,34 @@ for r in results:
             data[name] = [[int(y1), text]]
     
     process_data(data)
+import json
+
+from docx import Document
+import os
+
+# Mở tài liệu Word
+
+path = r'word_form/xinduong.docx'
+
+def check_exists(path):
+    exists = os.path.exists(path)
+    if exists:
+        return True
+    else:
+        return False
+
+def template(json_string):
+    data = json.loads(json_string)
+
+    print(data['type'])
+
+def main():
+    return 0
+
+
+# doc.save('bang_lai_da_dien_thong_tin.docx')
+
+if __name__ == '__main__':
+    json_string = '{"type": "Mặt sau", "name": "PHAM DUY LONG", "recent_location": "S Trà Co, Thanh Cái, Qung NInh phó Móng Khu Trang Ginl Trà Co, Thanh Móng Cál, phó", "birth_day": "03/12/2006", "id": "022206004066", "expire_date": "0v12/2031", "nationality": "Việt Nam", "origin_location": "Hải Xuan, Thành phố Móng Cái, Quảng Ninh Hải Xuán, Thành phó Móng Cá", "gender": "Nam"}'
+    template(json_string)
+
