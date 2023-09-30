3349dc33275db8291fd9d7595bb38abce623ee04
Hiro <80455873+zzhunght@users.noreply.github.com>
2023-09-30 08:30:01 +0700

    Squashed commit of the following:
    commit aade83b0b48bed43235fc67080f5893f3b819884
    Author: Hiro <80455873+zzhunght@users.noreply.github.com>
    Date:   Sat Sep 30 08:25:48 2023 +0700
    
        update ignore
    
    commit 6d9ca5e0ee3effb6e2be9b601161d2a4b496fb34
    Author: ZettourNguyen <121755160+ZettourNguyen@users.noreply.github.com>
    Date:   Sun Sep 24 20:48:55 2023 +0700
    
        merge main
    
    commit 9c413db9459c0cbf37bd4b2d659603fe6b8f2fc3
    Author: ZettourNguyen <121755160+ZettourNguyen@users.noreply.github.com>
    Date:   Sun Sep 24 20:47:39 2023 +0700
    
        complete
    
    commit c1e69863e0cbe120036ad82ce74003ee9e7a49c4
    Author: ZettourNguyen <121755160+ZettourNguyen@users.noreply.github.com>
    Date:   Sun Sep 24 20:43:05 2023 +0700
    
        complete
    
    commit c935086ebe91fbdc185ee3a1ef0c2b94ba6cc462
    Author: ZettourNguyen <121755160+ZettourNguyen@users.noreply.github.com>
    Date:   Mon Sep 18 16:38:42 2023 +0700
    
        add don_xin_tam_vang func -> word
    
    commit 87d501f910ed5654ecb5d9fad8e6311460ff55d0
    Author: ZettourNguyen <121755160+ZettourNguyen@users.noreply.github.com>
    Date:   Mon Sep 18 15:54:30 2023 +0700
    
        fix bug run.bold ==  none, add new word don_xin_dk_tam_tru
    
    commit 67f285730c2588000d8ddaf4725b672ec3f2ef97
    Author: ZettourNguyen <121755160+ZettourNguyen@users.noreply.github.com>
    Date:   Mon Sep 18 09:19:51 2023 +0700
    
        +2 at word_form
    
    commit 9b2be153dd15de817c0e4ae2bfe7222b220baaa4
    Author: ZettourNguyen <121755160+ZettourNguyen@users.noreply.github.com>
    Date:   Sun Sep 17 19:53:13 2023 +0700
    
        commit main
    
    commit 9da86e283847ca39f974384e0bba3d5df3954f16
    Author: ZettourNguyen <121755160+ZettourNguyen@users.noreply.github.com>
    Date:   Sun Sep 17 19:25:55 2023 +0700
    
        Initial commit
    
    commit 0012f5a9811a656bd7604385681985bc7e3f90b3
    Author: ZettourNguyen <121755160+ZettourNguyen@users.noreply.github.com>
    Date:   Sun Sep 17 19:33:05 2023 +0700
    
        complete excel and 1 word

---
+++ word.py
@@ -0,0 +1,129 @@
+import json
+from docx import Document
+import os
+from docx.shared import Pt, RGBColor
+
+
+def check_exists(path):
+    exists = os.path.exists(path)
+    if exists:
+        print('true')
+        return True
+    else:
+        print('false')
+        return False
+
+def so_yeu_ly_lich(path, json_string):
+    doc = Document(path)
+    style = doc.styles['Normal']
+    font = style.font
+    font.name = 'Time New Roman'
+    font.size = Pt(12)
+    data = json.loads(json_string)
+    birth_day = data['birth_day'].split('/')
+    expire_date = data['expire_date'].split('/') # this is expride_date,
+
+    for para in doc.paragraphs:
+        for run in para.runs:
+            if run.bold is not True:
+                para.text = para.text.replace('Số chứng minh…………………..', 'Số chứng minh……' + data['id']+ '.....')
+
+                para.text = para.text.replace('Họ và tên (chữ in hoa)………………………………………', 'Họ và tên (chữ in hoa).....'+ data['name']+ '...')
+                para.text = para.text.replace('. Sinh ngày……tháng …..năm …………', '. Sinh ngày ' +
+                               birth_day[0] + ' tháng ' + birth_day[1] + ' năm ' + birth_day[2] + '   ')
+                para.text = para.text.replace('Nam/ Nữ ….…….……........', 'Nam/ Nữ …..' + data['gender'])
+                para.text = para.text.replace('Nguyên quán……', 'Nguyên quán…' + data['origin_location'])
+                para.text = para.text.replace('Chỗ ở hiện nay ……', 'Chỗ ở hiện nay...' + data['recent_location'])
+                # # test expire_data == issued_data
+                para.text = para.text.replace('cấp ngày .…/…./……',
+                               'cấp ngày ' + expire_date[0] + '/' + expire_date[1] + '/' + expire_date[2])
+                # # para.text = para.text.replace('cấp ngày .…/…./……', 'cấp ngày '+ issued_date[0]+'/'+issued_date[1]+'/'+ issued_date[2])
+    doc.save(f'word_save/so-yeu-ly-lich-{data["name"]}.docx')
+
+def don_xin_dk_tam_tru(path, json_string):
+    doc = Document(path)
+    # style = doc.styles['Normal']
+    # font = style.font
+    # font.name = 'Time New Roman'
+    # font.size = Pt(12)
+    data = json.loads(json_string)
+
+    birth_day = data['birth_day'].split('/')
+    expire_date = data['expire_date'].split('/')  # this is expride_date,
+
+    for para in doc.paragraphs:
+        for run in para.runs:
+            if run.bold is not True:
+                para.text = para.text.replace('Số CCCD: ................................. ', 'Số CMND: ..' + data['id']+ '...')
+                para.text = para.text.replace('Tôi tên là: ....', 'Tôi tên là: ..' + data['name'])
+                para.text = para.text.replace('Ngày sinh:....', 'Ngày sinh:..' +
+                                              birth_day[0] + '/' + birth_day[1] + '/' + birth_day[2] + '   ')
+                para.text = para.text.replace('Địa chỉ thường trú......',
+                                              'Địa chỉ thường trú......' + data['recent_location'])
+                # test expire_data == issued_data
+                para.text = para.text.replace('Ngày:..................................................',
+                                              'Ngày:...' + expire_date[0] + '/' + expire_date[1] + '/' + expire_date[2])
+                # para.text = para.text.replace('Cấp tại:.....', 'Cấp tại:.....'+ issued_place[0]+'/'+issued_place[1]+'/'+ issued_date[2])
+    doc.save(f'word_save/don_xin_dk_tam_tru-{data["name"]}.docx')
+
+def don_xin_tam_vang(path, json_string):
+    doc = Document(path)
+    style = doc.styles['Normal']
+    font = style.font
+    font.name = 'Time New Roman'
+    font.size = Pt(12)
+    data = json.loads(json_string)
+    birth_day = data['birth_day'].split('/')
+    expire_date = data['expire_date'].split('/')  # this is expride_date,
+
+    for para in doc.paragraphs:
+        for para in doc.paragraphs:
+            for run in para.runs:
+                if run.bold is not True:
+                    para.text = para.text.replace('Số CCCD:................',
+                                                  'Số CCCD: ....' + data['id'])
+                    para.text = para.text.replace('Tôi tên là: ....', 'Tôi tên là: ..' + data['name'])
+                    para.text = para.text.replace('Ngày sinh:....', 'Ngày sinh:..' +
+                                                  birth_day[0] + '/' + birth_day[1] + '/' + birth_day[2] + '   ')
+                    para.text = para.text.replace('Địa chỉ thường trú:.....',
+                                                  'Địa chỉ thường trú:...' + data['recent_location'])
+                    # test expire_data == issued_data
+                    para.text = para.text.replace('Ngày:.....',
+                                                  'Ngày:...' + expire_date[0] + '/' + expire_date[1] + '/' +
+                                                  expire_date[2])
+                    # para.text = para.text.replace('Cấp tại:.....', 'Cấp tại:.....'+ issued_place[0]+'/'+issued_place[1]+'/'+ issued_date[2])
+    doc.save(f'word_save/don_xin_tam_vang-{data["name"]}.docx')
+
+json_string = ('{"type": "Mặt sau", '
+               '"name": "PHAM DUY LONG", '
+               '"recent_location": "S Trà Co, Thanh Cái, '
+               'Qung NInh phó Móng Khu Trang Ginl Trà Co, Thanh Móng Cál, phó", '
+               '"birth_day": "03/12/2006", '
+               '"id": "022206004066", '
+               '"expire_date": "01/12/2031", '
+               '"nationality": "Việt Nam", '
+               '"origin_location": "Hải Xuan, Thành phố Móng Cái, Quảng Ninh Hải Xuán, Thành phó Móng Cá", '
+               '"gender": "Nam"}')
+
+json_string2 = json.dumps({'type': 'Căn cược công dân mặt trước', 'recent_location': '8, Văn Thân 103/28, Văn Thân 103/28 Phường 08, Quận 6, TP. Hồ Chí Minh', 'name': 'PHAN NGỌC TRÂM', 'id': '079192019848', 'expire_date': '06/06/2032', 'birth_day': '06/06/1992', 'origin_location': 'Phường 08, Quận 6, TP.Hồ Chí Minh Phường 08, Quận 6, TP', 'nationality': 'Việt Nam', 'gender': 'Nữ'})
+# {'type': 'Căn cược công dân mặt trước',
+# 'recent_location': '8, Văn Thân 103/28, Văn Thân 103/28 Phường 08, Quận 6, TP. Hồ Chí Minh',
+# 'name': 'PHAN NGỌC TRÂM',
+# 'id': '079192019848',
+# 'expire_date': '06/06/2032',
+# 'birth_day': '06/06/1992',
+# 'origin_location': 'Phường 08, Quận 6, TP.Hồ Chí Minh Phường 08, Quận 6, TP', '
+# nationality': 'Việt Nam', '
+# gender': 'Nữ'}
+
+
+# ======PATH=====
+path_so_yeu_ly_lich = r'word_form/so-yeu-ly-lich.docx'
+path_don_xin_dk_tam_tru = r'word_form/don_xin_dk_tam_tru.docx'
+path_don_xin_tam_vang = r'word_form/don_xin_tam_vang.docx'
+
+# ======Call Function=====
+so_yeu_ly_lich(path_so_yeu_ly_lich, json_string2)
+don_xin_dk_tam_tru(path_don_xin_dk_tam_tru, json_string2)
+don_xin_tam_vang(path_don_xin_tam_vang, json_string2)
+
