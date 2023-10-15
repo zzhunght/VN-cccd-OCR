1eb3cbab1a3847b67664f9d58702ccdcb5b72dcd
Hiro <80455873+zzhunght@users.noreply.github.com>
2023-10-15 22:15:51 +0700

    Squashed commit of the following:
    commit 2cbaba46e4ed645e967b2a8c8439e27696e440e4
    Author: lieuminhkha <lieuminhkha@gmail.com>
    Date:   Sun Oct 15 22:11:13 2023 +0700
    
        fix ui
    
    commit f8421968f2384523fce2a3634dd9e98349d248d5
    Author: lieuminhkha <lieuminhkha@gmail.com>
    Date:   Mon Sep 25 01:02:23 2023 +0700
    
        fix ui
    
    commit 05b44ebafc33d3312a3e4abd35cf6de88e840182
    Author: lieuminhkha <lieuminhkha@gmail.com>
    Date:   Sun Sep 24 22:34:47 2023 +0700
    
        text

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,63 +1,49 @@
-"use client"
-import { useState } from 'react';
-import Introduction from '../../Components/Introduction'
-import Upload from '../../Components/Upload'
-import SliderForm from '../../Components/SliderForm';
-import Guide from '../../Components/Guide';
+"use client";
+import { useState, useRef } from "react";
+import Introduction from "../../Components/Introduction";
+import Upload from "../../Components/Upload";
+import SliderForm from "../../Components/SliderForm";
+import Guide from "../../Components/Guide";
 export default function Home() {
   const [imageSrc, setImageSrc] = useState([]);
-  const [selected,setSelected]=useState(-1)
-  const handleDownload = async (e:MouseEvent) =>{
-    console.log(selected) //lấy chỉ mục slider
-    console.log(imageSrc) //ảnh cccd //xử lí khi bấm tải về biểu mẫu thông tin
-    setSelected(-1) //xong rồi thì reset
-    setImageSrc([])
-  }
-  const handleExportExcel = async (e:MouseEvent) =>{
-    console.log(convertToBase64().chip_front64)
-    const data = {chip_front : convertToBase64()}
-    
-    try {
-      const response = await fetch('http://127.0.0.1:8000/excel-one', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-      });
-  
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-  
-      const responseData = await response.json();
-      console.log('Success:', responseData);
-    } catch (error) {
-      console.error('Error:', error);
+  const [selected, setSelected] = useState(-1);
+  const [showForm, setShowForm] = useState(false);
+  const handleDownload = async (e: MouseEvent) => {
+    setShowForm(true);
+  };
+  const handleExportExcel = async (e: MouseEvent) => {
+    if (imageSrc.length !== 2) {
+      alert("thiếu ảnh kìa thằng lozz"); // lát tao làm warning sau
     }
-  }
-
-  
-  function convertToBase64() {
-    const chip_front64 =  (imageSrc[0] as string).split(',')[1];
-    const chip_back64 =  (imageSrc[1] as string).split(',')[1];
-
-    return {
-      chip_front64,
-      chip_back64
-    };
-  }
-
+    console.log(imageSrc); //data là 2 file ảnh lưu trong biến imageSrc
+    //xử lí ở đây
+    setImageSrc([]); // xong rồi reset lại
+  };
   return (
-    <div className='content'>
-      <div className=' flex-center justify-between  '>
-        <Introduction/>
-        <Upload imageSrc={imageSrc} setImageSrc={setImageSrc}/>
+    <div className="content">
+      {showForm ? (
+        <SliderForm
+          setSelected={setSelected}
+          selected={selected}
+          showForm={showForm}
+          setShowForm={setShowForm}
+          imageSrc={imageSrc}
+          setImageSrc={setImageSrc}
+        />
+      ) : (
+        ""
+      )}
+      <div>
+        <Introduction />
+        <Upload
+          imageSrc={imageSrc}
+          setImageSrc={setImageSrc}
+          handleDownload={handleDownload}
+          handleExportExcel={handleExportExcel}
+        />
       </div>
-      <div className='flex flex-row-reverse  mt-2'>      
-        <SliderForm handleDownload={handleDownload} handleExportExcel={handleExportExcel} setSelected={setSelected} selected={selected}/>
-      </div>
-      <Guide/>
+      <div className="flex flex-row-reverse  mt-2"></div>
+      <Guide />
     </div>
-  )
+  );
 }
