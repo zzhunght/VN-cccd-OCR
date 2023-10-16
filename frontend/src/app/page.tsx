77407bcb36362db0936fcfa82aaff18bace42bd5
Hiro <80455873+zzhunght@users.noreply.github.com>
2023-10-16 21:59:17 +0700

    Squashed commit of the following:
    commit 7b9e3f194c888fc4012adb75de2156e27fb6a9b8
    Author: ZettourNguyen <121755160+ZettourNguyen@users.noreply.github.com>
    Date:   Mon Oct 16 08:34:50 2023 +0700
    
        bug 118 SliderForm.tsx
    
    commit 2424cfdd56d9a620eeac34ed39a4509c89e32dd5
    Author: ZettourNguyen <121755160+ZettourNguyen@users.noreply.github.com>
    Date:   Mon Oct 16 08:19:59 2023 +0700
    
        add /excel-existing, fix excel, fix word
    
    commit a5d4edb4bf1f9bce961f8caaa9bc194a87ba08ac
    Author: ZettourNguyen <121755160+ZettourNguyen@users.noreply.github.com>
    Date:   Sun Oct 15 23:57:11 2023 +0700
    
        add back_string to word

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -11,13 +11,42 @@ export default function Home() {
   const handleDownload = async (e: MouseEvent) => {
     setShowForm(true);
   };
+
+  function convertToBase64() {
+    const chip_front64 =  (imageSrc[0] as string).split(',')[1];
+    const chip_back64 =  (imageSrc[1] as string).split(',')[1];
+
+    return {
+      chip_front64,
+      chip_back64
+    };
+  }
+
   const handleExportExcel = async (e: MouseEvent) => {
-    if (imageSrc.length !== 2) {
-      alert("thiếu ảnh kìa thằng lozz"); // lát tao làm warning sau
+    // if (imageSrc.length !== 2) {
+    //   alert("thiếu ảnh kìa thằng lozz"); // lát tao làm warning sau
+    // } WARNING: lát tao làm warning sau
+    const data = {chip_front : convertToBase64().chip_front64, chip_back : convertToBase64().chip_back64}
+    
+    try {
+      const response = await fetch('http://127.0.0.1:8000/excel-one', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      });
+  
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+  
+      const responseData = await response.json();
+      console.log('Success:', responseData);
+    } catch (error) {
+      console.error('Error:', error);
     }
-    console.log(imageSrc); //data là 2 file ảnh lưu trong biến imageSrc
-    //xử lí ở đây
-    setImageSrc([]); // xong rồi reset lại
+    // setImageSrc([]); // xong rồi reset lại
   };
   return (
     <div className="content">
