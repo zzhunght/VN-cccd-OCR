10b3f66881767b90ddb69b05813a7c8e3bd68d94
ZettourNguyen <121755160+ZettourNguyen@users.noreply.github.com>
2023-10-21 22:52:49 +0700

    cay vai, nghi ngoi

--- frontend/src/app/Home.tsx
+++ frontend/src/app/Home.tsx
@@ -1,10 +1,11 @@
 "use client";
-import { useState, useRef } from "react";
+import { useState } from "react";
 import Introduction from "../../Components/Introduction";
 import Upload from "../../Components/Upload";
 import SliderForm from "../../Components/SliderForm";
 import Guide from "../../Components/Guide";
 import Warning from "./../../Components/Warning";
+
 export default function Home() {
   const [message, setMessage] = useState("");
   const [imageSrc, setImageSrc] = useState([""]);
@@ -18,12 +19,12 @@ export default function Home() {
   };
 
   function convertToBase64() {
-    const chip_front64 = (imageSrc[0] as string).split(',')[1];
-    const chip_back64 = (imageSrc[1] as string).split(',')[1];
+    const chip_front64 = (imageSrc[0] as string).split(",")[1];
+    const chip_back64 = (imageSrc[1] as string).split(",")[1];
 
     return {
       chip_front64,
-      chip_back64
+      chip_back64,
     };
   }
 
@@ -39,28 +40,33 @@ export default function Home() {
         setLastClickTime(currentTime);
       }
     }
-    
-    const data = { chip_front: convertToBase64().chip_front64, chip_back: convertToBase64().chip_back64 }
+    // if (imageSrc.length !== 2) {
+    //   alert("thiếu ảnh kìa thằng lozz"); // lát tao làm warning sau
+    // } WARNING: lát tao làm warning sau
+    const data = {
+      chip_front: convertToBase64().chip_front64,
+      chip_back: convertToBase64().chip_back64,
+    };
 
     try {
-      const response = await fetch('http://127.0.0.1:8000/excel-one', {
-        method: 'POST',
+      const response = await fetch("http://127.0.0.1:8000/excel-one", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json'
+          "Content-Type": "application/json",
         },
-        body: JSON.stringify(data)
+        body: JSON.stringify(data),
       });
 
       if (!response.ok) {
-        throw new Error('Network response was not ok');
+        throw new Error("Network response was not ok");
       }
 
       const responseData = await response.json();
-      console.log('Success:', responseData);
+      console.log("Success:", responseData);
     } catch (error) {
-      console.error('Error:', error);
+      console.error("Error:", error);
     }
-  }
+  };
   return (
     <div className="content">
       {warning ? <Warning setWarning={setWarning} message={message} /> : ""}
